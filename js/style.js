87a9d3c3fef8bd1bf7e2aedb210c77465b088edb
abhishelf <imabhishek.dev@gmail.com>
2022-10-09 01:18:05 +0530

    Fixing Navigation & Rollback the Navigation Bar Style

--- js/style.js
+++ js/style.js
@@ -31,11 +31,53 @@
           }, 000);
         });
 
-      
-      $(window).scroll(function(){
-          if ($(window).scrollTop() > 200) {
-            $("nav").addClass("menu_background")
-          } else {
-            $("nav").removeClass("menu_background")
-          }
+        var app = (function() {
+        var body = undefined;
+        var menu = undefined;
+        var menuItems = undefined;
+
+        var init = function init() {
+          body = document.querySelector("body");
+          menu = document.querySelector(".menu-icon");
+          menuItems = document.querySelectorAll(".nav__list-item");
+
+          applyListeners();
+        };
+
+        var applyListeners = function applyListeners() {
+          menu.addEventListener("click", function() {
+            return toggleClass(body, "nav-active");
+          });
+        };
+
+        $(".nav__list-item").on("click", function() {
+          $("body").removeClass("nav-active");
+        });
+
+        var toggleClass = function toggleClass(element, stringClass) {
+          if (element.classList.contains(stringClass))
+            element.classList.remove(stringClass);
+          else element.classList.add(stringClass);
+        };
+
+        init();
+      })();
+
+      $(".bg-nav").scroll({
+        direction: "both",
+        scrollOffset: 0,
+        scrollRatio: 0,
+        scrollingOutOfView: function(
+          elem,
+          offset,
+          direction,
+          coords,
+          scrollRatio,
+          thisTop,
+          winPos
+        ) {
+          var bgColor = elem.data("background");
+
+          $(".menu-icon__line").css("background-color", bgColor);
+        }
       });
